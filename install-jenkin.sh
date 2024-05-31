18ac4f69bf98f5e3a12953065a4661106d939167
Sot Sirymony <sotsirymony@Sots-MacBook-Pro.local>
2024-05-31 21:09:41 +0700

    added files

--- install-jenkin.sh
+++ install-jenkin.sh
@@ -2,25 +2,33 @@
 ssh root@your_droplet_ip
 #3.Update and Install Necessary Packages:
 #Once logged in, update your package lists:
+# Update the package index
 sudo apt update
-#Install Java, as Jenkins requires Java to run:
-sudo apt install adoptopenjdk-21-hotspot
-#java --version
+
+# Install OpenJDK 11 (if JDK 21 is not available)
+sudo apt install openjdk-11-jdk -y
+
+# Check Java version
 java -version
-#4.Download and Install Jenkins:
-#Add the Jenkins repository key to your system:
+
+# Download and import the Jenkins repository key
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-#Append the Debian package repository address to the server's sources.list
+
+# Add the Jenkins repository to the system's sources list
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-#Update your package lists again:
+
+# Update the package index again
 sudo apt update
-#Install Jenkins:
+
+# Install Jenkins
 sudo apt install jenkins -y
-#5.Start Jenkins:
-#Once installed, start the Jenkins service
+
+# Start Jenkins service
 sudo systemctl start jenkins
-#Enable Jenkins to start on boot
+
+# Enable Jenkins to start on boot
 sudo systemctl enable jenkins
+
 #Configure Jenkins:
 #Once logged in, configure Jenkins according to your requirements. This might include setting up agents, configuring security, adding build jobs, etc.
 
