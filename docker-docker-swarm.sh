f9e60ea06d48b09ee83720147c21f6cfb2936175
Sot Sirymony <sotsirymony@Sots-MacBook-Pro.local>
2024-05-31 16:57:26 +0700

    added files

--- docker-docker-swarm.sh
+++ docker-docker-swarm.sh
@@ -10,13 +10,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt-get install docker-ce
+docker --version
 #3.Start and Enable Docker Service
 #Start the Docker service and enable it to start on boot
 sudo systemctl start docker
 sudo systemctl enable docker
 #4.Install Docker Swarm (Optional)
-docker swarm init --advertise-addr <MANAGER_IP>
+#Run the following command on your terminal:
+# run bellow command base on ip found
+docker swarm init --advertise-addr <DROPLET_IP>
 #5.Join Additional Nodes to the Swarm (Optional):
-docker swarm join --token <TOKEN> <MANAGER_IP>:<PORT>
+docker swarm join --token <TOKEN> <DROPTLET_IP>:<PORT>
 #6.Verify Docker Swarm Setup
 docker node ls
+
+###docker-swarm1-joned-token noted
+## docker swarm join --token SWMTKN-1-52arptrew5kffnr50pjt9knvgx354jyx4ybp1palpto4nu6s4e-4c3piqoi3qixn8v2oxn7pxd9k 64.226.67.29:2377
