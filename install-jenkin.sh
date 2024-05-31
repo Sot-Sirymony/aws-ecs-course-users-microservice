d95ef50a661ecd7c5af0290af8de713da1537374
Sot Sirymony <sotsirymony@Sots-MacBook-Pro.local>
2024-05-31 21:26:25 +0700

    added files

--- install-jenkin.sh
+++ install-jenkin.sh
@@ -3,19 +3,26 @@ ssh root@your_droplet_ip
 #3.Update and Install Necessary Packages:
 #Once logged in, update your package lists:
 # Update the package index
-sudo apt update
+# Update the package index
+# Update the package index
+# Update the package index
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Install OpenJDK 11 (if JDK 21 is not available)
-sudo apt install openjdk-11-jdk -y
+sudo apt install openjdk-21-jdk -y
 
 # Check Java version
 java -version
 
 # Download and import the Jenkins repository key
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 # Add the Jenkins repository to the system's sources list
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Update the package index again
 sudo apt update
@@ -23,11 +30,8 @@ sudo apt update
 # Install Jenkins
 sudo apt install jenkins -y
 
-# Start Jenkins service
-sudo systemctl start jenkins
 
-# Enable Jenkins to start on boot
-sudo systemctl enable jenkins
+
 
 #Configure Jenkins:
 #Once logged in, configure Jenkins according to your requirements. This might include setting up agents, configuring security, adding build jobs, etc.
